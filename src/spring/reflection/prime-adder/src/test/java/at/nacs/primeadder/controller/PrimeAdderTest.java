cd651fea6c3bf7712f5a4e1a0346bdf8b82abfa1
Ashkan <ashkan.pb@gmail.com>
2019-03-01 19:07:23 +0100

    prime adder test

--- src/spring/reflection/prime-adder/src/test/java/at/nacs/primeadder/controller/PrimeAdderTest.java
+++ src/spring/reflection/prime-adder/src/test/java/at/nacs/primeadder/controller/PrimeAdderTest.java
@@ -1,12 +1,31 @@
 package at.nacs.primeadder.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class PrimeAdderTest {
 
-    @Test
-    void calculateSum() {
+    @Autowired
+    PrimeAdder primeAdder;
+
+    @ParameterizedTest
+    @CsvSource({
+            "5,2",
+            "10,3",
+            "17,4",
+            "2,1",
+            "24133,100",
+    })
+    void calculateSum(int expected, int limit) {
+        int actual = primeAdder.calculateSum(limit);
+        Assertions.assertEquals(expected, actual);
     }
 }
