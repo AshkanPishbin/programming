c5f7c45185cd4f801dee223ae6303e0b6b63ed5b
Ashkan <ashkan.pb@gmail.com>
2019-03-01 19:06:36 +0100

    primes test

--- src/spring/reflection/prime-adder/src/test/java/at/nacs/primeadder/controller/PrimesTest.java
+++ src/spring/reflection/prime-adder/src/test/java/at/nacs/primeadder/controller/PrimesTest.java
@@ -1,10 +1,33 @@
 package at.nacs.primeadder.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class PrimesTest {
 
-    @org.junit.jupiter.api.Test
-    void isPrime() {
+    @Autowired
+    Primes primes;
+
+    @ParameterizedTest
+    @CsvSource({
+            "true, 2",
+            "true, 5",
+            "false, 4",
+            "false, -1",
+            "false, 0",
+            "false, 1",
+
+    })
+    void isPrime(boolean expected, int candidate) {
+        boolean actual = Primes.isPrime(candidate);
+        Assertions.assertEquals(expected,actual);
+
+
     }
 }
