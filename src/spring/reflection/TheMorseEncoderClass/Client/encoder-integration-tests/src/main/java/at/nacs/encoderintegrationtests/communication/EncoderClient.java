7cf43ff05ece6bd8d99a83cdeec877bbd48cddc8
Ashkan <ashkan.pb@gmail.com>
2019-03-15 12:55:37 +0100

    week4

--- src/spring/reflection/TheMorseEncoderClass/Client/encoder-integration-tests/src/main/java/at/nacs/encoderintegrationtests/communication/EncoderClient.java
+++ src/spring/reflection/TheMorseEncoderClass/Client/encoder-integration-tests/src/main/java/at/nacs/encoderintegrationtests/communication/EncoderClient.java
@@ -1,28 +1,21 @@
-package at.nacs.encoder;
+package at.nacs.encoderintegrationtests.communication;
 
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/encode")
 @RequiredArgsConstructor
-public class EncoderEndpoint {
+public class EncoderClient {
 
     private final RestTemplate restTemplate;
 
-    @Value("${morse}")
+    @Value("${encoder.url}")
     private String url;
 
-    @GetMapping("/{message}")
-    String message(@PathVariable String message) {
+    public String encode(String message) {
         return restTemplate.postForObject(url, message, String.class);
     }
+
 }
-
-
--- src/spring/reflection/TheMorseEncoderClass/Encoder/src/main/java/at/nacs/encoder/RestTemplateConfiguration.java
+++ src/spring/reflection/TheMorseEncoderClass/Encoder/src/main/java/at/nacs/encoder/RestTemplateConfiguration.java
@@ -1,4 +1,13 @@
 package at.nacs.encoder;
 
-public class RestTemplateConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+    @Configuration
+    public class RestTemplateConfiguration {
+        @Bean
+        RestTemplate restTemplate() {
+            return new RestTemplate();
+        }
 }
