c81c3976f5bd5b1c98bac78c34fed5271e47a70c
Ashkan <ashkan.pb@gmail.com>
2019-03-16 13:45:34 +0100

    morseencoder

---
+++ src/spring/reflection/morseencoder/morse/src/main/java/at/nacs/morse/MorseEndpoint.java
@@ -0,0 +1,17 @@
+package at.nacs.morse;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/morse")
+public class MorseEndpoint {
+    @PostMapping
+    String letter(@RequestBody String letter) {
+        return null;
+
+    }
+}
+
